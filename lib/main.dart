5ea0df9d60d13e9322617cd08f70b6f577121c03
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2023-04-16 01:17:11 +0300

    text color to white

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,11 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         //TODO: Step 1 - Add background.png to this Container as a background image.
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage('images/background.png'), fit: BoxFit.cover),
         ),
-        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
@@ -42,7 +42,7 @@ class _StoryPageState extends State<StoryPage> {
                   child: Text(
                     //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storyBrain.getStory(),
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 25.0,
                     ),
                   ),
@@ -51,7 +51,7 @@ class _StoryPageState extends State<StoryPage> {
               Expanded(
                 flex: 2,
                 child: TextButton(
-                  style: ButtonStyle(
+                  style: const ButtonStyle(
                     backgroundColor:
                         MaterialStatePropertyAll<Color>(Colors.red),
                   ),
@@ -65,36 +65,41 @@ class _StoryPageState extends State<StoryPage> {
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(),
-                    style: TextStyle(
+                    style: const TextStyle(
+                      color: Color(0xFFFFFFFF),
                       fontSize: 20.0,
                     ),
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20.0,
               ),
               Expanded(
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll<Color>(
-                        Color.fromARGB(255, 221, 222, 144)),
-                  ),
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    setState(() {
-                      storyBrain.nextStory(2);
-                    });
-                  },
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    storyBrain.getChoice2(),
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    child: Text(
+                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
+                      style: const TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,7 @@
 import 'story.dart';
 
 class StoryBrain {
-  List<Story> _storyData = [
+  final List<Story> _storyData = [
     Story(
         storyTitle:
             'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
@@ -33,7 +33,13 @@ class StoryBrain {
         choice2: '')
   ];
 
-  int storyNumber = 0;
+  int _storyNumber = 0;
+
+  int get storyNumber => _storyNumber;
+
+  set storyNumber(int value) {
+    _storyNumber = value;
+  }
 
   String getStory() {
     return _storyData[storyNumber].storyTitle;
@@ -67,6 +73,14 @@ class StoryBrain {
 
   void restart() {
     storyNumber = 0;
+  }
+
+  bool buttonShouldBeVisible() {
+    if (storyNumber == 0 || storyNumber == 1 || storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
